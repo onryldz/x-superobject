bb24c015b3355b0cc29fa9a3b6873d72aa82bd49
onryldz10@gmail.com <onryldz10@gmail.com@f946fdf3-a2d1-f3ec-e497-be348cb20f6c>
2014-09-29 08:55:23 +0000

    Fixes issue 42 Fixes issue 43

--- trunk/XSuperJSON.pas
+++ trunk/XSuperJSON.pas
@@ -571,7 +571,7 @@ const
   optStop = optWhiteSpace + optSym;
 
   HexMap : array [0..15] of WideChar = ('0', '1', '2', '3', '4', '5', '6',
-           '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
+           '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
 
 var
   JSONLexGrammar: TJSONGrammar;
